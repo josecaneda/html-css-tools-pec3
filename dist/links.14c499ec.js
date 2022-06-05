43fb513f08774ab82376989c24f09cd8b3ed7b2c
jose caneda <jcaneda@uoc.edu>
2022-06-05 22:12:26 +0200

    p15

---
+++ dist/links.14c499ec.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.addEventListener("click", (event)=>{
+        let iframe = document.createElement('iframe');
+        iframe.src = `https://www.youtube.com/embed/${event.target.id}?autoplay=1`;
+        iframe.setAttribute('allow', 'accelerometer', 'autoplay', 'clipboard-write', 'encrypted-media', 'gyroscope', 'picture-in-picture');
+        iframe.setAttribute('allowFullScreen', '');
+        event.target.parentElement.replaceChild(iframe, event.target);
+    });
+};
+
+//# sourceMappingURL=links.14c499ec.js.map
--- src/app/links.js
+++ src/app/links.js
@@ -2,10 +2,10 @@ window.onload = function() {
     document.addEventListener("click", (event) => {
         let iframe = document.createElement('iframe');
         iframe.src = `https://www.youtube.com/embed/${event.target.id}?autoplay=1`;
-        iframe.setAttribute('allow', 'autoplay');
+        iframe.setAttribute('allow', 'accelerometer', 'autoplay', 'clipboard-write', 'encrypted-media', 'gyroscope', 'picture-in-picture');
+        iframe.setAttribute('allowFullScreen', '');
         event.target.parentElement.replaceChild(iframe, event.target);
     });
 }
-        
 
        
