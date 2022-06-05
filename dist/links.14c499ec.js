8787563b26e1cccc9caffa28648792363461e897
jose caneda <jcaneda@uoc.edu>
2022-06-05 13:45:09 +0200

    p10

---
+++ dist/links.14c499ec.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    document.addEventListener("click", (event)=>{
+        let iframe = document.createElement('iframe');
+        iframe.src = `https://www.youtube.com/embed/${event.target.id}?autoplay=1`;
+        event.target.parentElement.replaceChild(iframe, event.target);
+    });
+};
+
+//# sourceMappingURL=links.14c499ec.js.map
